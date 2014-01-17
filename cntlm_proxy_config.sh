6633615abaf0d1e02e17a9e48892b587bd9160f5
John O'Gara <johnogara@gmail.com>
2014-01-17 20:34:32 +0000

    shouldn't include all those vim plugins

--- cntlm_proxy_config.sh
+++ cntlm_proxy_config.sh
@@ -3,10 +3,15 @@ export http_proxy="http://localhost:3128"
 export https_proxy=$http_proxy
 }
 
-function home_proxy {
+function home {
 unset http_proxy
 unset https_proxy
 }
 
-work_proxy
+if [[ `hostname` == *think*  ]]
+then
+  home
+else
+  work_proxy
+fi
 
