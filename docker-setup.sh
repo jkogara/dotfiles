def90adc97db426a1a5ff8d782bb17b1236da985
John O'Gara <johnogara@gmail.com>
2017-01-22 18:03:09 +0000

    docker conveniences for aws

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-docker-machine start
 eval "$(docker-machine env default)"
+
+function docker-login-personal(){
+  `aws ecr get-login --region us-east-1 --profile personal`
+}
+
+function docker-tag-latest-personal(){
+  docker tag swiss-pairing:latest 055257691102.dkr.ecr.us-east-1.amazonaws.com/swiss-pairing:latest
+}
+
+function docker-push-personal(){
+  docker push 055257691102.dkr.ecr.us-east-1.amazonaws.com/swiss-pairing:latest
+}
