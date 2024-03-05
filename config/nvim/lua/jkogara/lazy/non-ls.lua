0cadff8b325b605f409590913a47374d8113daa7
John O'Gara <johnogara@gmail.com>
2024-03-05 12:24:08 +0000

    Don't use prettier for javascript or typescript

--- config/nvim/lua/jkogara/lazy/non-ls.lua
+++ config/nvim/lua/jkogara/lazy/non-ls.lua
@@ -1,61 +1,59 @@
 return {
-  {
-    "nvimtools/none-ls.nvim",
-    config = function()
-      local null_ls = require("null-ls")
-      local sources = {
-        null_ls.builtins.code_actions.eslint_d,
-        null_ls.builtins.completion.luasnip,
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.diagnostics.flake8,
-        null_ls.builtins.diagnostics.rubocop,
-        null_ls.builtins.diagnostics.terraform_validate,
-        null_ls.builtins.diagnostics.tsc,
-        null_ls.builtins.diagnostics.write_good,
-        null_ls.builtins.formatting.black.with({ extra_args = { "--fast" } }),
-        null_ls.builtins.formatting.fixjson,
-        null_ls.builtins.formatting.isort,
-        null_ls.builtins.formatting.prettier.with({
-          filetypes = {
-            "javascript",
-            "typescript",
-            "css",
-            "scss",
-            "html",
-            "json",
-            "yaml",
-            "markdown",
-            "graphql",
-            "md",
-            "txt",
-          },
-        }),
-        null_ls.builtins.formatting.rubocop,
-        null_ls.builtins.formatting.rustfmt,
-        null_ls.builtins.formatting.shfmt,
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.terraform_fmt,
-      }
-      null_ls.setup({
-        sources = sources,
-        debug = true,
+	{
+		"nvimtools/none-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
+			local sources = {
+				null_ls.builtins.code_actions.eslint_d,
+				null_ls.builtins.completion.luasnip,
+				null_ls.builtins.diagnostics.eslint_d,
+				null_ls.builtins.diagnostics.flake8,
+				null_ls.builtins.diagnostics.rubocop,
+				null_ls.builtins.diagnostics.terraform_validate,
+				null_ls.builtins.diagnostics.tsc,
+				null_ls.builtins.diagnostics.write_good,
+				null_ls.builtins.formatting.black.with({ extra_args = { "--fast" } }),
+				null_ls.builtins.formatting.fixjson,
+				null_ls.builtins.formatting.isort,
+				null_ls.builtins.formatting.prettier.with({
+					filetypes = {
+						"css",
+						"scss",
+						"html",
+						"json",
+						"yaml",
+						"markdown",
+						"graphql",
+						"md",
+						"txt",
+					},
+				}),
+				null_ls.builtins.formatting.rubocop,
+				null_ls.builtins.formatting.rustfmt,
+				null_ls.builtins.formatting.shfmt,
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.terraform_fmt,
+			}
+			null_ls.setup({
+				sources = sources,
+				debug = true,
 
-        on_attach = function(client, bufnr)
-          if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              group = augroup,
-              buffer = bufnr,
-              callback = function()
-                vim.lsp.buf.format({ async = false })
-              end,
-            })
-          end
-        end,
-      })
-    end,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-  },
+				on_attach = function(client, bufnr)
+					if client.supports_method("textDocument/formatting") then
+						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							group = augroup,
+							buffer = bufnr,
+							callback = function()
+								vim.lsp.buf.format({ async = false })
+							end,
+						})
+					end
+				end,
+			})
+		end,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
 }
