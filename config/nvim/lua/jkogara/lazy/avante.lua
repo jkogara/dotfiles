b52ee4ce77ba0d1a9bc91db4d77a268abdc19946
John O'Gara <johnogara@gmail.com>
2025-05-27 16:44:27 +0100

    Update to latest avante

--- config/nvim/lua/jkogara/lazy/avante.lua
+++ config/nvim/lua/jkogara/lazy/avante.lua
@@ -24,7 +24,8 @@ return {
       claude = {
         endpoint = "https://api.anthropic.com",
         -- model = "claude-3-5-sonnet-20241022",
-        model = "claude-3-7-sonnet-20250219",
+        -- model = "claude-3-7-sonnet-20250219",
+        model = "claude-sonnet-4-20250514",
         timeout = 30000, -- Timeout in milliseconds
         temperature = 0,
       },
